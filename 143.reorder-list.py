be8ce1502ecd2d41abaa0b67b118747e0e908e7c
Taea <88346289+adrastaea@users.noreply.github.com>
2024-01-27 15:07:48 -0500

    linear complexity

--- 143.reorder-list.py
+++ 143.reorder-list.py
@@ -13,6 +13,53 @@
 class Solution:
 
     def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+
+        # half the List
+        slow = head
+        fast = head
+        prev = None # add pointer to track end of first half
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        # disconnect first half from second half
+        prev.next = None
+
+        # reverse second half
+        prev = None # start at none to have an end to reversed list
+        curr = slow # start curr at first node
+        # once curr is None, the prev node is the new beginning
+        while curr:
+            # store the next node before changing pointer to next
+            next_node = curr.next
+            # point curr at prev
+            curr.next = prev
+            # advance prev and curr
+            prev = curr
+            curr = next_node
+        # notate new reversed list
+        r_curr = prev
+
+        # Merge
+        prev = head
+        curr = head.next
+        while curr:
+            # store next nodes
+            next_node = curr.next
+            r_next_node = r_curr.next
+            # insert r_curr
+            prev.next = r_curr
+            r_curr.next = curr
+            # advance prev, curr, r_curr
+            prev = curr
+            curr = next_node
+            r_curr = r_next_node
+        prev.next = r_curr
+
+    def reorderListnSquared(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
